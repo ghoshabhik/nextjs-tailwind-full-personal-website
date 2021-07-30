43ee6930badf036dc626088e52d2986fe3ae74d6
abhik <avik5324@gmail.com>
2021-07-30 21:06:01 +0530

    added seo meta info

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import {useState} from 'react'
+import { useRouter } from 'next/router';
 
 // import TopNav from '../../components/layout/TopNav'
 import TopNavigation from '../../components/layout/TopNavigation'
@@ -9,6 +10,7 @@ export default function Container(props) {
 
     const [currentMode, setCurrentMode] = useState("close")
     const {children} = props
+    const router = useRouter();
 
     const toggleMode = () => {
         if(currentMode === 'close'){
@@ -18,12 +20,36 @@ export default function Container(props) {
         }
     }
 
+    const meta = {
+        title: 'Abhik Ghosh – Developer, architect, writer.',
+        description: `A data solution architect, Cloud enthusiast and course creator.`,
+        image: 'https://abhikghosh.in/images/site/banner.png',
+        type: 'website'
+      };
+
     return (
         <>
             <Head>
-                <title>Abhik Ghosh | Personal Website</title>
-                <meta name="description" content="Personal website to showcase my work, projects and writing" />
                 <link rel="icon" href="/images/site/favicon.ico" />
+
+                <title>{meta.title}</title>
+                <meta name="robots" content="follow, index" />
+                <meta content={meta.description} name="description" />
+                <meta property="og:url" content={`https://abhikghosh.in${router.asPath}`} />
+                <link rel="canonical" href={`https://abhikghosh.in${router.asPath}`} />
+                <meta property="og:type" content={meta.type} />
+                <meta property="og:site_name" content="Abhik Ghosh" />
+                <meta property="og:description" content={meta.description} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:image" content={meta.image} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@avik5324" />
+                <meta name="twitter:title" content={meta.title} />
+                <meta name="twitter:description" content={meta.description} />
+                <meta name="twitter:image" content={meta.image} />
+                {meta.date && (
+                <meta property="article:published_time" content={meta.date} />
+                )}
             </Head>
             <TopNavigation toggleMode={toggleMode} currentMode={currentMode}/>
             
